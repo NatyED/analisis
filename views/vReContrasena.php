d40ed5077837d15807291db86014f9d27eacf7dd
Natali ED <natali27899@gmail.com>
2021-08-05 15:08:08 -0600

    vista recuperar contraseña

---
+++ views/vReContrasena.php
@@ -0,0 +1,42 @@
+<section class="min-vh-100 d-flex justify-content-center align-items-center">
+    <form class="d-block row  w-50" id="FormularioRecuperarContrasena">
+        <div class="col-12 my-5 text-center">
+            <h1>Recuperar Contraseña</h1>
+        </div>
+        <div class="col-12 ">
+            <div class="mb-3">
+                <label for="" class="form-label">Numero de Identificacion o pasaporte:</label>
+                <input class="form-control" name="perfilIdentificacion" type="text" disabled placeholder="" aria-label="">
+            </div>
+        </div>
+        
+        <div class="col-12 ">
+            <div class="mb-3">
+                <label for="" class="form-label">Correo Electrónico:</label>
+                <input class="form-control" name="correoPerfil" type="email" placeholder="" aria-label="">
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="mb-3">
+                <label for="" class="form-label">Telefono:</label>
+                <input class="form-control" name="telefonoPerfil" type="number" placeholder="" aria-label="">
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="mb-3">
+                <label for="" class="form-label">Contraseña nueva:</label>
+                <input class="form-control" name="contrasenaNueva" type="password" placeholder="" aria-label="">
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="mb-3">
+                <label for="" class="form-label">Verificar contraseña nueva:</label>
+                <input class="form-control" name="verificarNewContrasena" type="password" placeholder="" aria-label="">
+            </div>
+        </div>
+       
+        <div class="col-12 mt-5 d-flex justify-content-evenly my-5">
+            <button type="button" class="btn bg-dark text-light btnRecuperarContrasena ">Lo que sea</button>
+
+    </form>
+</section>
